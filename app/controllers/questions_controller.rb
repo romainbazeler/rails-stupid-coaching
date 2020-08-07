f930b52dfd9ef114d9ddc78ec7f40dd5f7731a02
romainbazeler <romain.bazeler@hotmail.com>
2020-08-06 20:20:02 -0400

    add answer page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,18 @@
 class QuestionsController < ApplicationController
     def ask
-  
+    end
+
+    def answer
+        if params[:answer]== "i am going to work right now!"
+            @answer =""
+        elsif params[:answer].end_with?("?")
+          @answer = "Silly question, get dressed and go to work!"
+        else
+            @answer ="I don't care, get dressed and go to work!"
+        end
+        return @answer
     end
 end
+
+
+
